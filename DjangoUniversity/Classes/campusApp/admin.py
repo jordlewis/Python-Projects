bdc1cbb8c7704815afd4daa5214a3bf8d0496bde
jordlewis <ohgoodjord@gmail.com>
2023-03-27 09:22:11 -0500

    Basic Python Projects
    This repository is for the basic python projects I've completed.

--- DjangoUniversity/Classes/campusApp/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import UniversityCampus
-
-# Registering the 'UniversityCampus' model
-admin.site.register(UniversityCampus)
--- DjangoUniversity/Classes/classApp/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import UniversityClasses
-
-# Registering the 'UniversityClasses' model
-admin.site.register(UniversityClasses)
